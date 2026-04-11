fcd5175fd86e8474ab8ad535a1c1fbb5acc76086
Vreckan <ewqqwe550005@yahoo.com.tw>
2026-04-11 11:24:22 +0800

    remove error annotation

--- src/hw5Game/RoleGameTest.java
+++ src/hw5Game/RoleGameTest.java
@@ -5,7 +5,7 @@ public class RoleGameTest {
 	public static void main(String[] args) {
 		Hero saber = new SwordMan("亞拉岡", 1, 0); // 晉升全部都是Hero型別
 		Hero archer = new ArrowMan("勒苟拉斯", 1, 0);
-		Hero priest  = new PriestMan();// 設計:父類別第一轉是法師 後續可以轉弓箭/劍士
+		Hero priest = new PriestMan();
 
 		Hero[] HeroArray = new Hero[3];
 
