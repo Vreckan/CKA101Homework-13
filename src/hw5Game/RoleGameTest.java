b5e0dc6b838a1ff7767b7b9ac8a7c8f6b39170c8
Vreckan <ewqqwe550005@yahoo.com.tw>
2026-04-11 09:15:45 +0800

    RoleGame final

--- src/hw5Game/RoleGameTest.java
+++ src/hw5Game/RoleGameTest.java
@@ -8,12 +8,12 @@ public class RoleGameTest {
 		
 		Hero [] HeroArray = new Hero[2];
 		HeroArray [0] = saber;
-		HeroArray [1] = archer;
+		saber.setattack(new AttackSword());
+		
+		HeroArray  [1] = archer;
+		archer.setattack(new AttackArrow());
+		
 		for(int i = 0 ; i < HeroArray.length ; i ++) {
-			if(i==0)
-				HeroArray[i].setattack(new AttackSword());
-			else
-				HeroArray[i].setattack(new AttackArrow());
 			HeroArray[i].setdefend(new Defend());
 			HeroArray[i].setmove(new Run());
 			HeroArray[i].run();
