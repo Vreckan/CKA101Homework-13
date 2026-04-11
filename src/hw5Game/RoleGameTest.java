ff917ed90bd4d105fd4b8885381f898da7cbc3b9
Vreckan <ewqqwe550005@yahoo.com.tw>
2026-04-11 08:56:55 +0800

    midify RoleGameTest

--- src/hw5Game/RoleGameTest.java
+++ src/hw5Game/RoleGameTest.java
@@ -5,18 +5,20 @@ public class RoleGameTest {
 	public static void main(String[] args) {
 		Hero saber = new SwordMan("亞拉岡", 1, 0);
 		Hero archer = new ArrowMan("勒苟拉斯", 1, 0);
-		saber.setmove(new Run());
-		saber.setdefend(new Defend());
-		saber.setattack(new AttackSword());
-		saber.run();
-		saber.attack();
-		saber.defend();
 		
-		archer.setmove(new Run());
-		archer.setattack(new AttackArrow());
-		archer.setdefend(new Defend());
-		archer.run();
-		archer.attack();
-		archer.defend();
+		Hero [] HeroArray = new Hero[2];
+		HeroArray [0] = saber;
+		HeroArray [1] = archer;
+		for(int i = 0 ; i < HeroArray.length ; i ++) {
+			if(i==0)
+				HeroArray[i].setattack(new AttackSword());
+			else
+				HeroArray[i].setattack(new AttackArrow());
+			HeroArray[i].setdefend(new Defend());
+			HeroArray[i].setmove(new Run());
+			HeroArray[i].run();
+			HeroArray[i].attack();
+			HeroArray[i].defend();
+		}
 	}
 }
