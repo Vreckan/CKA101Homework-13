7f9094c2e01fdc408b5882a2eb3939ca37fb2589
Vreckan <ewqqwe550005@yahoo.com.tw>
2026-04-14 23:45:05 +0800

    refactor : extract mothod for input handing

--- src/hw6/CalTest.java
+++ src/hw6/CalTest.java
@@ -3,45 +3,39 @@ package hw6;
 import java.util.Scanner;
 
 public class CalTest {
+	final static int EXIT= 9527;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		System.out.println("如要離開請輸入9527");
+		
+		System.out.println("如要離開請輸入"+EXIT);
 
 		while (true) {
-			int x, y;
-
-			System.out.println("請輸入x變數");
-			if (!sc.hasNextInt()) {
-				System.out.println("格式不正確:這不是整數");
-				sc.next(); // 把錯誤輸入吃掉
-				continue;
-			} else {
-				x = sc.nextInt();
-				if (x == 9527)
-					break;
-			}
-
-			System.out.println("請輸入y指數");
-			if (!sc.hasNextInt()) {
-				System.out.println("格式不正確:這不是整數");
-				sc.next(); // 把錯誤輸入吃掉
-				continue;
-			} else {
-				y = sc.nextInt();
-				if (y == 9527)
-					break;
-			}
-
+			int x = readInt(sc,"請輸入x變數");
+			if(x==EXIT) break;
+			
+			int y = readInt(sc,"請輸入x變數");
+			if(y==EXIT) break;
+			
 			try {
 				Calculator obj1 = new Calculator(x, y);
 				System.out.println(obj1.powerXY());
 			} catch (CalException e) {
 				e.printStackTrace();
 			}
-
 		}
-		System.out.println("9527終止");
+		System.out.println("終止");
 		sc.close(); // 關閉底層資源 (System.in
+	}
+	
+	public static int readInt(Scanner sc,String str) {
+		while(true) {
+			System.out.println(str);
+			if(sc.hasNext()) {
+				return sc.nextInt();
+			}else {
+				System.out.println("格式不正確　：　這不是整數");
+				sc.next();
+			}
+		}
 	}
 }
